aedf2d5caee6ed858c00b67e7c1ed6a9a29eda1b
Pacifique Ntahurunga <np@npacifique.us>
2021-03-23 17:02:32 -0400

    feat : Big-O notation

---
+++ Arrays-Big-O Notation/src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static  void main(String[] args){
+        //array capacity is 5
+        int[] intArray = new int [5];
+
+        intArray[0] = 3;
+        intArray[1] = 8;
+        intArray[2] = -1;
+        intArray[3] = 23;
+        intArray[4] = 15;
+
+        //print array
+
+        for (int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+
+
+
+        /**
+            array memory  | x + i * y
+            x = memory address
+            i = index of element
+            y = size of element
+
+            finding the memory address an element
+            address of array[0] = 12 + (0 * 4) = 12
+            address of array[1] = 12 + (1 * 4) = 16
+            address of array[2] = 12 + (2 * 4) = 20
+            ...
+            address of array[5] = 12 + (5 * 4) = 32
+            note: the advantage of using arrays comes when the index is know and which provide a performance advantage
+            the Big-O notation O(1) / the retrieval happen in constant time
+
+         */
+
+        //searching throughout the array
+        int index = -1;
+        for (int i = 0; i < intArray.length; i++){
+            if(intArray[i] == 23){
+                index = i;
+                break;
+            }
+
+        }
+
+        System.out.println("index = "+ index);
+
+        /**
+            For Big-O notation we always consider the worse case
+            - in our example the worse case will be if 23 was at the end of the array which mean the search the whole array
+            - n = number of element
+            - Big-O O(n) / Linear time
+         */
+
+    }
+}
